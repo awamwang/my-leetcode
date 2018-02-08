df9b9fe812be4b5c66d7b979a1b37abaab5846a5
wangmeng <wangnew2013@126.com>
2018-02-08 15:02:13 +0800

    update

---
+++ Accepted/204. Count Primes.js
@@ -0,0 +1,15 @@
+// 204. Count Primes
+//
+// https://leetcode.com/problems/count-primes/description/
+//
+// Description:
+//
+// Count the number of prime numbers less than a non-negative number, n.
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var countPrimes = function(n) {
+
+};
